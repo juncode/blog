bfb9b49fa15c98ae879e519096ed8a835b66325d
xiaoan <xiaoan@juanpi.com>
2015-06-17 10:52:56 +0800

    更改数据配置

--- wp-config.php
+++ wp-config.php
@@ -15,16 +15,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'blog');
 
 /** MySQL数据库用户名 */
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'xxxxxxx');
+define('DB_PASSWORD', 'root');
 
 /** MySQL主机 */
-define('DB_HOST', 'xx.88');
+define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
